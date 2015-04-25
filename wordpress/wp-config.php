ef7f7fa59e977390c3c703914bdae82733ad42f1
socc726 <sam@nenvy.com>
2015-04-25 01:11:43 -0400

    added theme

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -68,8 +68,8 @@ $table_prefix  = 'wp_';
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  */
-define('WP_DEBUG', false);
-
+define('WP_DEBUG', true);
+define('WP_ENV', 'development');
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
