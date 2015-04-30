c8c530abee42a527df255f6f051007e7bb392c8e
socc726 <sam@nenvy.com>
2015-04-30 07:48:35 -0400

    contact form stuff

--- wordpress/wp-content/themes/kauffman-group/page.php
+++ wordpress/wp-content/themes/kauffman-group/page.php
@@ -11,13 +11,20 @@
  */
 
 get_header(); ?>
-	  <?php echo do_shortcode('[image-carousel interval="0" category="homepage" twbs="3" ]'); ?>
+<?php
+  if (is_front_page())
+    echo do_shortcode('[image-carousel interval="0" category="homepage" twbs="3" ]');
+
+?>
 	<!-- Main jumbotron for a primary marketing message or call to action -->
 	<div class="jumbotron">
 
 
 	  <div class="container">
 			<?php while ( have_posts() ) : the_post(); ?>
+			      	<div class="page-header">
+      		 <h2><?php the_title(); ?></h2>
+      	</div>
 				<?php get_template_part( 'content', 'page' ); ?>
 
 			<?php endwhile; // end of the loop. ?>
